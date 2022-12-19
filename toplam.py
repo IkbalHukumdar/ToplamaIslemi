a184a1ad7c51098e9c926d564a47d8e260c89c14
CemileGozalici <99996275+CemileGozalici@users.noreply.github.com>
2022-12-19 21:15:26 +0300

    Update toplam.py

--- toplam.py
+++ toplam.py
@@ -1,4 +1,15 @@
 sayi1=int(input("Sayı 1: "))
 sayi2=int(input("Sayı 2: "))
-toplam=sayi1+sayi2
-print("Toplam: ",toplam)
+
+print("Hangi işlemi yapmak istiyorsunuz (+,-,*,/): ")
+islem = input()
+if islem == '+':
+    print("\n" +str(sayi1)+ " + " +str(sayi2)+ " = " +str(sayi1+sayi2))
+elif islem == '-':
+    print("\n" +str(sayi1)+ " - " +str(sayi2)+ " = " +str(sayi1-sayi2))
+elif islem == '*':
+    print("\n" +str(sayi1)+ " * " +str(sayi2)+ " = " +str(sayi1*sayi2))
+elif islem == '/':
+    print("\n" +str(sayi1)+ " / " +str(sayi2)+ " = " +str(sayi1/sayi2))
+else:
+    print("\nBöyle bir işlem bulunmuyor.")
